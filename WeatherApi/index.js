b3d329daa3e7def07cc2b060cc3af1234cd65dba
Antonio Moscato <moscatoantono@gmail.com>
2024-04-23 11:00:56 +0200

    fit \connection to weather api

---
+++ WeatherApi/index.js
@@ -0,0 +1,6 @@
+let long = 0
+let lat = 0
+let startD = 0
+let endD = 0
+
+const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,wind_speed_10m,temperature_80m&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&start_date=2024-04-01&end_date=2024-04-23"
