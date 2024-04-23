d17988ddb5962bf7089d00137652972657c4ac84
Federico Balducci <ott8v@proton.me>
2024-04-23 12:44:36 +0200

    feat(dbApi/index.js): add example Schema

--- dbApi/index.js
+++ dbApi/index.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const prevSchema = new mongoose.Schema({
+    nameCity: String,
+    latitude: Number,
+    longitude: Number,
+    daily: Boolean,
+    date: String,
+});
+
+const Prev = mongoose.Model('Prev', prevSchema);
+
 async function connect(){
     try{
         await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PSWD}@mycluster.kvsrvry.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster/Weather`);
