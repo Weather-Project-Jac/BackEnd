e60b9d11797d283cd1b992f0404b1fd16181b2c6
Antonio Moscato <moscatoantono@gmail.com>
2024-04-23 12:48:26 +0200

    feat: add call to weather api

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 const { getWeather } = require("./WeatherApi/index.js")
+const express = require('express')
 
-getWeather().then(result => {
-  console.log(result)
+const port = 3000
+
+const app = express()
+const rUser = express.Router()
+const rWeather = express.Router()
+
+
+
+
+//send today weather
+rWeather.get("/", (req, res) => {
+
+  //TODO: controllo prima di fare la chiamata se il dato è presente nel db
+  getWeather().then(result => {
+    res.status(200).send(result)
+  })
+})
+
+//send range date weather
+rWeather.get("/", (req, res) => {
+
+  //TODO: controllo prima di fare la chiamata se il dato è presente nel db
+  getWeather("date1", "date2").then(result => {
+    res.status(200).send(result)
+  })
+})
+
+
+app.use("/user", rUser)
+app.use("/weather", rWeather)
+
+app.listen(port, () => {
+  console.log(port)
 })
