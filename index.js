cf750baad498eaff40800de25c2e6d5fa0d17ee5
Antonio Moscato <moscatoantono@gmail.com>
2024-04-23 16:11:36 +0200

    feat: add User in main Api

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const app = express()
 const rUser = express.Router()
 const rWeather = express.Router()
 
-
-
-
 //send today weather
 rWeather.get("/", (req, res) => {
 
@@ -28,6 +25,31 @@ rWeather.get("/", (req, res) => {
   })
 })
 
+
+//do a login
+rUser.get("/:mail/:psw", (req, res) => {
+  let mail = req.params.mail
+  let psw = req.params.psw
+
+  if (mail == undefined || psw == undefined) {
+    res.status(500).send("non sono state mandate mail e/o password")
+    return
+  }
+  //TODO: recuperare i dati di un eventuale utente
+})
+
+//register login
+rUser.post("/", (req, res) => {
+  let date = req.body
+
+  if (date["mail"] == undefined || date["psw"] == undefined) {
+    res.send(500).send("mail o psw non inserita")
+    return
+  }
+
+  //TODO: controlla se nel db è già presente un utente con quella mail
+  //TODO: crea il nuovo utente
+})
 
 app.use("/user", rUser)
 app.use("/weather", rWeather)
