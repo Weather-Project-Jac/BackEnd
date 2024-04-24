7e713e64a5208a8980b23bcd89700d1ea6f923d7
Federico Balducci <ott8v@proton.me>
2024-04-24 19:48:10 +0200

    feat(dbApi/Schema): create and define user.js

---
+++ dbApi/Schema/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { passportLocalMongoose} = require('passport-local-mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    saltRounds: { 
+        type: Number, 
+        default: 10 
+    },
+    email: { 
+        type: String, required: 
+        true, unique: true 
+    },
+    profile_image_url: { 
+        type: String, 
+        default: 'https://placehold.co/600x400' 
+    },
+    registered_at: { 
+        type: Date, 
+        default: Date.now 
+    },
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = {UserSchema};
