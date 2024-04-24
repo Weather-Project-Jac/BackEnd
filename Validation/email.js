6c226c23ba03a6ec3dc7bbf790dbfd1fe87c5f09
Antonio Moscato <moscatoantono@gmail.com>
2024-04-24 10:23:24 +0200

    fix: email validation

--- Validation/email.js
+++ Validation/email.js
@@ -1,14 +1,14 @@
 function mailValidation(mail) {
   if (mail.indexOf("@") == -1 || (mail.indexOf(".com") == -1 && mail.indexOf(".it") == -1)) {
-    return
+    return false
   }
 
   if (mail.indexOf("@") == (mail.indexOf(".com") && mail.indexOf(".it")) - 1) {
-    return
+    return false
   }
 
   if (mail.indexOf("@") == 0 || (mail.indexOf(".com") + 4 && mail.indexOf(".it") + 3) != mail.length) {
-    return
+    return false
   }
 
   return true
