28ae1fd68330960bfd5b8b6b7431bee43a7639bc
Antonio Moscato <moscatoantono@gmail.com>
2024-04-26 10:55:31 +0200

    feat: add find function in dbApi

--- dbApi/index.js
+++ dbApi/index.js
@@ -1,38 +1,39 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const {hourlyPrevSchema} = require('./Schema/hourlyPrev.js');
-const {dailyPrevSchema} = require('./Schema/hourlyPrev.js');
+const { hourlyPrevSchema } = require('./Schema/hourlyPrev.js');
+const { dailyPrevSchema } = require('./Schema/hourlyPrev.js');
 
-let db = {}; 
+const { getWeather } = require("../WeatherApi/index.js")
+
+let db = {};
 
 
-async function connect(){
-    try{
+async function connect() {
+    try {
+        console.log(process.env.USER)
         await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PSWD}@mycluster.kvsrvry.mongodb.net/Weather?retryWrites=true&w=majority&appName=MyCluster/Weather`);
         console.log("Connect: True")
-    }catch(err){
+    } catch (err) {
         console.log(err);
         return;
     }
-    
-    
 }
 
-async function disconnect(){
+async function disconnect() {
     await mongoose.connection.close();
     console.log("Connect: False");
 }
 
-async function addHourly(cityName, object){
-    const year = (object.daily.time[0]).substring(0,4);
+async function addHourly(cityName, object) {
+    const year = (object.daily.time[0]).substring(0, 4);
     const Model = mongoose.model(year, hourlyPrevSchema);
-    for(let i in object.hourly.time){
+    for (let i in object.hourly.time) {
         await Model.create({
-            cityName: cityName, 
+            cityName: cityName,
             latitude: object.latitude,
             longitude: object.longitude,
             daily: true,
-            date: object.hourly.time[i].substring(5,10),
+            date: object.hourly.time[i].substring(5, 10),
             hour: object.hourly.time[i].substring(11),
             data: {
                 relativeHumidity: object.hourly.relative_humidity_2m[i],
@@ -43,19 +44,19 @@ async function addHourly(cityName, object){
             }
         })
     }
-    
+
 }
 
-async function addDaily(cityName, object){
-    const year = (object.daily.time[0]).substring(0,4);
+async function addDaily(cityName, object) {
+    const year = (object.daily.time[0]).substring(0, 4);
     const Model = mongoose.model(year, dailyPrevSchema);
-    for(let i in object.daily.time){
+    for (let i in object.daily.time) {
         await Model.create({
-            cityName: cityName, 
+            cityName: cityName,
             latitude: object.latitude,
             longitude: object.longitude,
             daily: false,
-            date: object.hourly.time[i].substring(5,10),
+            date: object.hourly.time[i].substring(5, 10),
             data: {
                 temperatureMax: object.daily.temperature_2m_max[i],
                 temperatureMin: object.daily.temperature_2m_min[i]
@@ -64,14 +65,54 @@ async function addDaily(cityName, object){
     }
 }
 
-async function addPrevisons(cityName, object){
-    Promise.all([addHourly(cityName, object),addDaily(cityName, object)])
-    .then((result) => {
-        console.log("Sucess upload");
-    })
-    .catch((err) => {
-        console.error(err);
-    })
+async function addPrevisons(cityName, object) {
+    Promise.all([addHourly(cityName, object), addDaily(cityName, object)])
+        .then((result) => {
+            console.log("Sucess upload");
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+}
+
+async function findWeather(cityName, endD, startD = undefined) {
+    let schema = hourlyPrevSchema
+    let daily = true
+
+
+    let sd = undefined
+    let sy = undefined
+
+    if (startD != undefined) {
+        schema = dailyPrevSchema
+        daily = false
+
+        sy = startD.substring(0, 4)
+        sd = startD.substring(5, 10)
+    }
+
+    if ((startD != undefined && startD.length != 10) || endD.length != 10) {
+        return false
+    }
+
+    let year = endD.substring(0, 4)
+    let date = endD.substring(5, 10)
+
+    const Model = mongoose.model(year, schema);
+    let result
+    if (daily) {
+        result = await Model.find({ "daily": true, "date": date, "cityName": cityName })
+    } else {
+        result = await Model.find({
+            "daily": true, "cityName": cityName,
+            "date": {
+                $gte: sd,
+                $lt: date
+            }
+        })
+    }
+
+    return result
 }
 
 db.mongoose = mongoose;
@@ -81,4 +122,14 @@ db.connect = connect;
 db.disconnect = disconnect;
 db.addPrevisions = addPrevisons;
 
-module.exports = db; 
+module.exports = db;
+
+// let location = "Bergamo"
+// db.connect().then(() => {
+//     findWeather("Bergamo", "2024-04-26", "2024-03-26").then(element => {
+//         console.log("result:")
+//         console.log(element)
+//     }).catch(error => {
+//         console.log(error)
+//     })
+// })
