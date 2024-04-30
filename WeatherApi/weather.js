a8dc249872db312364e1bf18ead8addb6ec913ad
Antonio Moscato <moscatoantono@gmail.com>
2024-04-30 17:14:08 +0200

    fix: error in weather api

--- WeatherApi/weather.js
+++ WeatherApi/weather.js
@@ -5,17 +5,19 @@ const { getPosition } = require("./position")
 
 //definisco la funzione per recuperare il meteo
 async function getWeather(location, countryDate = undefined, startDate = undefined, endDate = undefined) {
-
+  let long = undefined
+  let lat = undefined
   //recupero dati della location
   let data = await getPosition(location, countryDate)
   console.log(data)
   if (data == undefined) {
     return false
   }
+
   try {
     //recupero latitudine e longitudine
-    let long = data["results"][0]["longitude"]
-    let lat = data["results"][0]["latitude"]
+    long = data["results"][0]["longitude"]
+    lat = data["results"][0]["latitude"]
   } catch (error) {
     console.log(error)
     return false
@@ -42,7 +44,6 @@ async function getWeather(location, countryDate = undefined, startDate = undefin
   //definisco l'URL per connettermi all'API
   const urlWeather = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + long + "&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,wind_speed_10m,temperature_80m&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&start_date=" + startD + "&end_date=" + endD
 
-  console.log("URLWE: " + urlWeather)
   //recupero tutti i dati ricevuti dalla chiamata
   let date = (await axios.get(urlWeather))["data"]
   return date
