3363691379007be043801869fb9ba7227d511862
Antonio Moscato <moscatoantono@gmail.com>
2024-05-07 09:24:37 +0200

    feat: add token

---
+++ API/jwt.js
@@ -0,0 +1,23 @@
+var jwt = require('jsonwebtoken');
+
+
+function createToken(object) {
+  if (object == undefined) {
+    return false
+  }
+  var token = jwt.sign({
+    data: object
+  }, "secretKeyForToken", { expiresIn: "3d" })
+  return token
+}
+
+function validateToken(token) {
+  try {
+    var decoded = jwt.verify(token, 'secretKeyForToken');
+    console.log(decoded)
+    return true
+  } catch (err) {
+    console.log("token errato")
+    return false
+  }
+}
